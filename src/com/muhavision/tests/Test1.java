d1c4e0697b78d656004f9379f325236b5ba6bdfd
Pylo <klemen.pylo@gmail.com>
2014-08-19 15:15:59 +0200

    First working commit

--- src/com/muhavision/tests/Test1.java
+++ src/com/muhavision/tests/Test1.java
@@ -1,5 +1,27 @@
 package com.muhavision.tests;
 
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_highgui.*;
+
+import org.bytedeco.javacv.CanvasFrame;
+
 public class Test1 {
 
+	
+	public static void main(String[] args) {
+        
+	     //Load image img1 as IplImage
+	        final IplImage image = cvLoadImage("img.png");
+	         
+	        //create canvas frame named 'Demo'
+	        final CanvasFrame canvas = new CanvasFrame("Demo");
+	         
+	        //Show image in canvas frame
+	        canvas.showImage(image);
+	         
+	        //This will close canvas frame on exit
+	        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);        
+	 }   
+	
 }
