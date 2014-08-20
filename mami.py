5ae3504cc727a4a44611403352b1c0cfd72072b8
Vid Drobnič <vid.drobnic@icloud.com>
2014-08-20 15:02:38 +0200

    Mami je nared!

---
+++ mami.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame import locals
+
+pygame.init()
+
+pygame.joystick.init() # main joystick device system
+
+try:
+	j = pygame.joystick.Joystick(0) # create a joystick instance
+	j.init() # init instance
+	print 'Enabled joystick: ' + j.get_name()
+except pygame.error:
+	print 'no joystick found.'
+
+while 1:
+	for e in pygame.event.get(): # iterate over event stack
+		#print 'event : ' + str(e.type)
+		if e.type == pygame.locals.JOYAXISMOTION: # 7
+			x , y, z, a = j.get_axis(0)*20, j.get_axis(1)*20, j.get_axis(2)*20, j.get_axis(3)
+			print str(x) + ';' + str(y) + ';' + str(z) + ';' + str(a)
+		#elif e.type == pygame.locals.JOYBALLMOTION: # 8
+		#	print 'ball motion'
+		#elif e.type == pygame.locals.JOYHATMOTION: # 9
+		#	print 'hat motion'
+		#elif e.type == pygame.locals.JOYBUTTONDOWN: # 10
+		#	print 'button down'
+		#elif e.type == pygame.locals.JOYBUTTONUP: # 11
+		#	print 'button up'
