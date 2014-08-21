0f0fe5da5d44b29e568ec5d51a2b54bd3f3eca65
zigapk <ziga.patacko@gmail.com>
2014-08-21 10:40:46 +0200

    expo mami added

---
+++ src/com/muhavision/control/ExpoController.java
@@ -0,0 +1,13 @@
+package com.muhavision.control;
+
+/**
+ * Created by ziga on 21.8.2014.
+ */
+public class ExpoController {
+
+
+    public static double getExpo(int power){
+        if (power<0) return-(0.6 * Math.exp(-0.035*(double)power) - 0.6);
+        return 0.6 * Math.exp(0.035*(double)power) - 0.6;
+    }
+}
