843e3769de705c53c05b584f7b1ed72d80c0bcee
Vid Drobnič <vid.drobnic@icloud.com>
2014-08-21 10:43:14 +0200

    mame prste proc!!!

--- src/com/muhavision/control/ExpoController.java
+++ src/com/muhavision/control/ExpoController.java
@@ -5,7 +5,7 @@ package com.muhavision.control;
  */
 public class ExpoController {
 
-    //If it crashes it's because of this.
+    //If it crashes it's because of this + mami ne dotike se.
     public static double getExpo(int power){
         if (power<0) return-(0.6 * Math.exp(-0.035*(double)power) - 0.6);
         return 0.6 * Math.exp(0.035*(double)power) - 0.6;
