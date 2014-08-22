dee7789e2b7dd53e1f36827be513d0a250a78cd9
Vid Drobnič <vid.drobnic@icloud.com>
2014-08-22 07:53:35 +0200

    Pidikus

--- src/com/muhavision/control/MarkerCalculator.java
+++ src/com/muhavision/control/MarkerCalculator.java
@@ -5,8 +5,8 @@ import com.muhavision.pid.PID;
 
 public class MarkerCalculator {
 	
-	static PID yaw = new PID(0.5f, 0, 0, 0, PID.Direction.NORMAL);
-	static PID pitch = new PID(1, 0, 0, 0, PID.Direction.NORMAL);
+	static PID yaw = new PID(0.25f, 0, 0, 0, PID.Direction.NORMAL);
+	static PID pitch = new PID(0.005f, 0, 0.0003f, 0, PID.Direction.NORMAL);
 	
 	public static void calculateAndControl(EulerAngles angles, Main main){
 		
@@ -37,7 +37,7 @@ public class MarkerCalculator {
 			
 			if(pxdist<80){
 				
-				float pitchout = (pitch.calculate(pxdist, 38))/2;
+				float pitchout = (pitch.calculate(pxdist, 40))/2;
 				
 				if(Math.abs(pitchout)<9){
 					main.pitch = pitchout*-1;
